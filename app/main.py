a56de4b414da9421fee425725d0d682611fbd75e
fanni <fmalevych@zoolatech.com>
2025-02-14 13:27:15 +0200

    add get weather function

--- app/main.py
+++ app/main.py
@@ -1,6 +1,35 @@
+import os
+import requests
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+WEATHER_URL = "http://api.weatherapi.com/v1/current.json"
+CITY = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    api_key = os.getenv("API_KEY")
+    if not api_key:
+        print("API_KEY environment variable not set.")
+    else:
+        try:
+            url = f"{WEATHER_URL}?key={api_key}&q={CITY}"
+            response = requests.get(url)
+
+            weather_data = response.json()
+
+            country = weather_data["location"]["country"]
+            localtime = weather_data["location"]["localtime"]
+            temperature = weather_data["current"]["temp_c"]
+            condition = weather_data["current"]["condition"]["text"]
+
+            print(f"Performing requests to Weather API for city {CITY}...")
+            print(f"{CITY}/{country} {localtime} - {temperature}°C, {condition}")
+
+        except requests.exceptions.RequestException as e:
+            print(f"Request error: {e}")
 
 
 if __name__ == "__main__":
